4b9235b8f8f378c530e1e52ee9ed12977089b16d
Senya Webster <senyawebster@Senyas-MacBook-Air-2.local>
2017-08-18 13:17:27 -0700

    got ping, pong, and pingpong working

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,39 @@
+
+//Back-End Logic
+var items = []
+
+function pingPong (number) {
+  // First if divisible by 15 b/c shortens two modulos to
+  // one, lowest common denominator between 3 and 5.
+  if (number % 15 === 0) {
+    items.push("<li>pingpong</li>");
+  } else if (number % 3 === 0) {
+    items.push("<li>ping</li>");
+  } else if (number % 5 === 0) {
+    items.push("<li>pong</li>");
+  } else {
+    items.push("<li>" + number + "</li>");
+  }
+}
+
+
+
+//Front-End Logic
 $(document).ready(function(){
-  $("____#________").submit(function(event){
+  $("form#formInput").submit(function(event) {
     event.preventDefault();
-    
+
+    var userInput = $("input#userInput").val();
+
+    pingPong(userInput);
+
+    $("ul#resultsList").empty();
+
+    for (i = 0; i < items.length; i++) {
+      $("ul#resultsList").append(items[i]);
+    }
+    // items.forEach(function(item) {
+    //   $("ul#resultsList").append(item);
+    // });
   });
 });
